a8a2096b0b1156437e5b081965fbd2cbdefdea5c
andrewni <ajni@ucdavis.edu>
2018-02-08 23:40:03 -0800

    added an openapi3 spec layout and unit testing example

--- classes/components.py
+++ classes/components.py
@@ -2,6 +2,10 @@
 We'll have to figure out how each of these are handled during code generation
 Dependency mapping is important, because inside each of their dictionaries, they can contain references to other class instances
 Unsure if these classes will be needed, but if anything should be in a class it should be these, because they're referenced everywhere in the spec
+
+PEP8 standards: https://stackoverflow.com/questions/4613000/what-is-the-cls-variable-used-for-in-python-classes/4795306#4795306
+Always use 'self' for the first argument to instance methods
+Always use 'cls' for the first argument to class methods
 """
 
 
