1628c77111d808802715b243d6321ab75edb3d3f
James Chen <jchen463@gmail.com>
2018-02-11 19:17:18 -0800

    added example flask server

---
+++ Rest_Flask_Example/todo_api/README.md
@@ -0,0 +1,5 @@
+In the todo_api directory, run the command 'python3 __main__.py'
+
+As of right now, only the getTasks work
+
+Check the requirements.txt for the needed python packages
---
+++ Rest_Flask_Example/todo_api/__main__.py
@@ -0,0 +1,14 @@
+#!venv/bin/python
+from flask import Flask
+from controllers.tasks_controller import tasks_api
+
+app = Flask(__name__)
+app.register_blueprint(tasks_api)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.register_blueprint(tasks_api)
+    app.run(debug=True)
