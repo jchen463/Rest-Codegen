614ac06835caf4089a0148729d52a4702dc65d82
andrewni <ajni@ucdavis.edu>
2018-02-16 00:24:48 -0800

    finished links

--- classes/link.py
+++ classes/link.py
@@ -1,22 +1,25 @@
 class Link:
     def __init__(self, dikt):
         from .parse import parse_dict
+        from .parse import get_mapping
 
-        # !!! parameters and requestBody can be <any | {expression}>
-        # probably will have to manually input parameters and requestBody
-        allowed = ['operationRef', 'operationId', 'parameters',
+        allowed = ['operationRef', 'operationId',
                    'requestBody', 'description', 'server',
                    'extensions']
         objects = ['server']
-        mappings = ['parameters']
 
         d = parse_dict(dikt=dikt, allowed=allowed, objects=objects,
                        mappings=mappings)
 
         self.operationRef = d['operationRef']
         self.operationId = d['operationId']
-        self.parameters = d['parameters']
         self.requestBody = d['requestBody']
         self.description = d['description']
         self.server = d['server']
         self.extensions = d['extensions']
+
+        self.parameters = None
+        if 'parameters' in dikt:
+            self.parameters = get_mapping('scopes', dikt['parameters'])
+
+        # !!! parameters and requestBody can be <any | {expression}>
