3b64c7a614752d2944490509ff87c0fd66303458
andrewni <ajni@ucdavis.edu>
2018-03-02 08:17:28 -0800

    removed unnecessary files

--- codegen/testing/A.py
+++
@@ -1,8 +0,0 @@
-iterators_mapping = {}
-iterator_functions_mapping = {}
-
-
-def codegen_stage(x_iterator, x_iterator_functions):
-    iterator_name = x_iterator.__name__
-    iterators_mapping[iterator_name] = x_iterator
-    iterator_functions_mapping[iterator_name] = x_iterator_functions
