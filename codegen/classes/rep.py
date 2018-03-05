f4e7ce5bef943685b5992de750286c984c7a5926
jchen463 <jchen463@gmail.com>
2018-03-04 18:06:53 -0800

    added my portion of refactoring of classes and changed rep.py

--- codegen/classes/rep.py
+++ codegen/classes/rep.py
@@ -1,3 +1,9 @@
 class Rep:
     def __repr__(self):
         return repr(vars(self))
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self == other
