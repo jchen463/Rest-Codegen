bfd2c27a21cdaf59d27bb54709f31e38cf3e6617
andrewni <ajni@ucdavis.edu>
2018-03-07 01:31:39 -0800

    moved flask tmeplates

--- codegen/main.py
+++ codegen/main.py
@@ -9,15 +9,15 @@ import argparse
 from openapi_spec_validator import openapi_v3_spec_validator
 
 try:  # when just doing $ python3 main.py only below imports work
-    from codegen.classes import Specification
     import codegen.codegen_config as cfg
+    from codegen.classes import Specification
     from codegen.flask_server_codegen import flask_server_codegen, stage_default_iterators
-    from codegen.typescript_client_codegen import typescript_client_codegen, stage_default_iterators
+    # from codegen.typescript_client_codegen import typescript_client_codegen, stage_default_iterators
 except ImportError as err:  # when packaged, only above imports work
+    import codegen_config as cfg
     from classes import Specification
     from flask_server_codegen import flask_server_codegen, stage_default_iterators
-    from typescript_client_codegen import typescript_client_codegen, stage_default_iterators
-    import codegen_config as cfg
+    # from typescript_client_codegen import typescript_client_codegen, stage_default_iterators
 
 
 def main():
@@ -42,7 +42,7 @@ def main():
         json.dump(spec_dict2, out, indent=4)
 
     flask_server_codegen()
-    typescript_client_codegen()
+    # typescript_client_codegen()
 
 
 def load_spec_file(file_path):
