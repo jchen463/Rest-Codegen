21e15ab96e9150bf3a3b074a931f969f7ec42bdc
Dylon Dickinson <dylondickinson@Dylons-MacBook-Pro.local>
2018-03-14 02:55:39 -0700

    client pytest

--- tests/pyTest/conftest.py
+++ tests/pyTest/conftest.py
@@ -1,13 +1,35 @@
 import pytest, os, sys
 
 @pytest.fixture(scope="module")
-def genServerFiles():
+def serverFiles():
 
     #specify custom build file here
     build_file = ''
     
+    assert os.system('codegen ' + build_file) == 0
+    
+    return project_info('flask-server-generated')
+        
+@pytest.fixture(scope="module")
+def clientFiles():
+
+    #specify custom build file here
+    build_file = 'build.py'
+    
+    assert os.system('ng new myproject') == 0
+    assert os.system('cp swagger.yaml build.py myproject/src') == 0
     try:
-        os.system('codegen ' + build_file)
+        os.chdir('myproject/src')
     except:
         assert 0
-      
+    assert os.system('codegen ' + build_file) == 0
+    try:
+        os.chdir('../..')
+    except:
+        assert 0
+    
+    return project_info('myproject')
+    
+class project_info():
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
