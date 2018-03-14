a14abbeb2d1bf2dd5e380902684deded0e5f4a57
Dylon Dickinson <dylondickinson@campus-034-001.ucdavis.edu>
2018-03-14 15:42:49 -0700

    testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,40 +1,41 @@
-# import pytest
-# import os
-# import sys
+ import pytest
+ import os
+ import sys
 
 
-# @pytest.fixture(scope="module")
-# def serverFiles():
+ @pytest.fixture(scope="module")
+ def serverFiles():
 
-#     # specify custom build file here
-#     build_file = ''
+     # specify custom build file here
+     build_file = ''
 
-#     assert os.system('codegen ' + build_file) == 0
+     assert os.system('codegen ' + build_file) == 0
 
-#     return project_info('flask-server-generated')
-
-
-# @pytest.fixture(scope="module")
-# def clientFiles():
-
-#     # specify custom build file here
-#     build_file = 'build.py'
-
-#     assert os.system('ng new myproject') == 0
-#     assert os.system('cp swagger.yaml build.py myproject/src') == 0
-#     try:
-#         os.chdir('myproject/src')
-#     except:
-#         assert 0
-#     assert os.system('codegen ' + build_file) == 0
-#     try:
-#         os.chdir('../..')
-#     except:
-#         assert 0
-
-#     return project_info('myproject')
+     return project_info('flask-server-generated')
 
 
-# class project_info():
-#     def __init__(self, root_dir):
-#         self.root_dir = root_dir
+ @pytest.fixture(scope="module")
+ def clientFiles():
+
+     # specify custom build file here
+     build_file = 'build.py'
+
+     assert os.system('ng new myproject') == 0
+     assert os.system('cp swagger.yaml build.py myproject/src') == 0
+     try:
+         os.chdir('myproject/src')
+     except:
+         assert 0
+     assert os.system('codegen ' + build_file) == 0
+     try:
+         os.chdir('../..')
+     except:
+         assert 0
+
+     return project_info('myproject')
+
+
+ class project_info():
+     def __init__(self, root_dir):
+         self.root_dir = root_dir
+
--- tests/test_files_not_empty.py
+++ tests/test_files_not_empty.py
@@ -1,24 +1,25 @@
-# import pytest
-# import os
-# import sys
+ import pytest
+ import os
+ import sys
 
 
-# def files_not_empty(root_dir):
-#     path = os.path.join(os.getcwd(), root_dir)
-#     print('\n\nfiles under ' + root_dir + '\n\n')
-#     for (path, dirs, files) in os.walk(path):
-#         for f in files:
-#             fp = os.path.join(path, f)
-#             size = os.path.getsize(fp)
-#             print(fp)
-#             print('size: ' + str(size) + ('' if size != 0 else '        ****empty****'))
+ def files_not_empty(root_dir):
+     path = os.path.join(os.getcwd(), root_dir)
+     print('\n\nfiles under ' + root_dir + '\n\n')
+     for (path, dirs, files) in os.walk(path):
+         for f in files:
+             fp = os.path.join(path, f)
+             size = os.path.getsize(fp)
+             print(fp)
+             print('size: ' + str(size) + ('' if size != 0 else '        ****empty****'))
 
-#     return True
-
-
-# def test_server(serverFiles):
-#     assert files_not_empty(serverFiles.root_dir) is True
+     return True
 
 
-# def test_client(clientFiles):
-#     assert files_not_empty(clientFiles.root_dir) is True
+ def test_server(serverFiles):
+     assert files_not_empty(serverFiles.root_dir) is True
+
+
+ def test_client(clientFiles):
+     assert files_not_empty(clientFiles.root_dir) is True
+
