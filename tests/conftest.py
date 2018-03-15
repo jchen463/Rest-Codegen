6b0ea96e592ffb5d3681c60367462696075b6033
Dylon Dickinson <dylondickinson@campus-034-001.ucdavis.edu>
2018-03-14 17:02:00 -0700

    test broken

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,41 +1,50 @@
- import pytest
- import os
- import sys
+import pytest
+import os
+import sys
 
 
- @pytest.fixture(scope="module")
- def serverFiles():
+@pytest.fixture(scope="module")
+def serverFiles():
+ 
+    assert os.path.split(os.getcwd())[1] == 'tests'
 
-     # specify custom build file here
-     build_file = ''
+    # specify custom build file here
+    build_file = ''
 
-     assert os.system('codegen ' + build_file) == 0
+    assert os.system('codegen ' + build_file) == 0
 
-     return project_info('flask-server-generated')
-
-
- @pytest.fixture(scope="module")
- def clientFiles():
-
-     # specify custom build file here
-     build_file = 'build.py'
-
-     assert os.system('ng new myproject') == 0
-     assert os.system('cp swagger.yaml build.py myproject/src') == 0
-     try:
-         os.chdir('myproject/src')
-     except:
-         assert 0
-     assert os.system('codegen ' + build_file) == 0
-     try:
-         os.chdir('../..')
-     except:
-         assert 0
-
-     return project_info('myproject')
+    return project('flask-server-generated')
 
 
- class project_info():
-     def __init__(self, root_dir):
-         self.root_dir = root_dir
+@pytest.fixture(scope="module")
+def clientFiles():
+
+    # specify custom build file here
+    build_file = 'build.py'
+    yaml_file = 'swagger.yaml'
+
+    assert os.system('cp specs/' + yaml_file + ' ' + build_file + ' myproject/src') == 0
+    try:
+        os.chdir('myproject/src')
+    except:
+        assert 0
+    assert os.system('codegen ' + 'builds/' + build_file) == 0
+    try:
+        os.chdir('../..')
+    except:
+        assert 0
+
+    return project('myproject')
+     
+
+
+
+class project():
+    
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+    
+    def __del__(self):
+        assert os.system('rm -rf ' + self.root_dir)
+      
 
